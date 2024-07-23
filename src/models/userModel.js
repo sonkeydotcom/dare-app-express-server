ca9b8528b4f8ee9fc4d0f9f383cde3be0fce0c2e
unknown <annagu.kennedy@gmail.com>
2024-07-23 19:23:47 +0100

    User authentication

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -17,6 +17,11 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    role: {
+      type: String,
+      default: "user",
+      enum: ["user", "admin"],
+    },
   },
   {
     timestamps: true,
